cb8c8060fae1a75902f1357e8201726df739091b
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-05-27 21:59:42 +0200

    Erstell 3x3-Board, setz Playerzeichen, console.log
    Es wird das Board erstellt, indem ein leeres Array
    dem initialisiert wird.
    Das Board erhält 3x3 Zellen.
    Anschließend prüft eine Methode, ob die Zelle, die
    sich ein Player aussucht frei ist und setzt somit
    dessen Zeichen rein.
    Das Board wird aktualisiert in der Konsole
    wiedergegeben

--- script.js
+++ script.js
@@ -1,2 +1,91 @@
 'use strict';
 
+// Gameboard mit Spielbrett innerhalb
+function gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [
+
+    ];
+
+    // Erstelle in einem Durchlauf Reohen und innerhalb dieser mit einem weiteren Durchlauf Spalten,
+    // sodass ein 3x3 Raster entsteht -> also das board-Objekt soll 3 reihen X 3 Spalten haben
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(cell());
+        }
+    }
+
+    // Nachdem Spielbrett erstellt wurde, gib es zurück
+    const getBoard = () => {
+        board;
+    }
+
+
+    // je nach dem, welcher Spieler dran ist (den suchst du aus)
+    // => den Player in eine Zelle setzen (column und dazugehörige row aussuchen)
+
+    // iteriere über alle zellen und prüfe, ob dein ausgewählte zelle frei ist
+    // wenn ja, setze player rein
+
+    const setToken = (column, row, player) => {
+        const availableCells = board.filter(() => {
+            row[column].getValue() === 0
+        }).map(() => {
+            row[column];
+        })
+
+        if (!availableCells.length) {
+            return
+        }
+        else {
+            board[row][column].addToken(player);
+        }
+    }
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => {
+            row.map((cell => {
+                cell.getValue()
+            }))
+        })
+        console.log(boardWithCellValues);
+    }
+
+    return {
+        getBoard,
+        setToken,
+        printBoard
+    }
+}
+
+// Factory-Function zur Erstellung von Zellen
+function cell() {
+    let value = 0;
+
+    // für einen Spieler entscheiden und dementsprechend value aktualisieren
+    const addToken = (player) => {
+        value = player;
+    }
+
+    const getValue = () => {
+        value;
+    }
+
+    return {
+        addToken,
+        getValue
+    }
+}
+
+
+// Spieler - Factory-Function
+
+
+
+// Spielablauf - Factory-Function
+
+
+
+// Aufruf der Factory-Function für den Spielablauf
